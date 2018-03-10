45f8cc1807f7316a1ccc1af34297bc0bb2220f44
Jenkins <jenkins@pawo-partners.fr>
2018-03-10 01:44:25 +0100

    ajout d'un front end + plugin maven

---
+++ cdl-business/src/main/java/config/BusinessConfig.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessConfig.class, args);
+    }
+}
---
+++ cdl-rest/src/main/java/config/RestConfig.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestConfig.class, args);
+    }
+}
