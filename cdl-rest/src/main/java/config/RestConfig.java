8f10b1e5a7343ec528bc58435f75da47126cc49d
Jenkins <jenkins@pawo-partners.fr>
2018-03-10 16:13:33 +0100

    création d'un web service rest + ajout du proxy pour frontend

--- cdl-rest/src/main/java/config/RestConfig.java
+++ cdl-rest/src/main/java/config/RestConfig.java
@@ -1,9 +1,16 @@
 package config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "rest.controller")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class RestConfig {
 
     public static void main(String[] args) {
