1a475c090a8627ee07e3e36bb2f083efdfe5a765
mohamed laaroussi <dali.laaroussi@gmail.com>
2018-03-12 09:55:37 +0100

    first commit

--- cdl-core/src/main/java/fr/pawo/partners/cdl/security/JWTAuthorizationFilter.java
+++ cdl-core/src/main/java/fr/pawo/partners/cdl/security/JWTAuthorizationFilter.java
@@ -2,5 +2,23 @@ package fr.pawo.partners.cdl.security;
 
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain)
+                                    throws ServletException, IOException {
+
+        String jwtToken = request.getHeader(SecurityConstants.HEADER_STRING);
+        if(jwtToken==null || !jwtToken.startsWith(SecurityConstants.TOKEN_PREFIX)){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+    }
 }
